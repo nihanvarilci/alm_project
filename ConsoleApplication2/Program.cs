dc82be3f59a9dae105eba051de120030be568f9f
ugurakkurt <ugur.akkurt@ug.bilkent.edu.tr>
2016-11-26 19:27:10 +0300

    division operation on integers

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -15,6 +15,27 @@ namespace ConsoleApplication2
             String x = "welcome to calculator";
             Console.WriteLine("Hello {0}", x);
             Console.WriteLine("ASD");
+            double result;
+            Console.WriteLine("Enter your choice(divide: 1)");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                try
+                {
+                    int a, b;
+                    Console.WriteLine("Enter first number: ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second: ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    result = (double)a / b;
+                    Console.WriteLine("Result " + result);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error");
+                    throw;
+                }
+            }
         }
     }
 }

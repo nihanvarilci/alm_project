0b6b7a4bb756f23554a7f8c9846200b30b80f875
ugurakkurt <ugur.akkurt@ug.bilkent.edu.tr>
2016-12-14 23:33:02 +0200

    just a comment:(

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            ////////
+            ///////////
             Program p1 = new ConsoleApplication2.Program(); // deneme 
             String yourname = "Mr. Nice";                       // deneme 2
             Console.WriteLine("Hello {0}", yourname);       //deneme 3
